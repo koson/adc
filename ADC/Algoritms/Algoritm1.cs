04563da10f77aa1691dee4f5b5b3290fc802cf17
unknown <Simeon Scherbinin>
2014-06-29 14:36:03 +0400

    2 algoritm

--- ADC/Algoritms/Algoritm1.cs
+++ ADC/Algoritms/Algoritm1.cs
@@ -8,9 +8,9 @@ namespace adc.Algoritms
 {
 	public class Algoritm1
 	{
-		private  double _p0;
-		private  int _n;
-		private  double _gamma;
+		private double _p0;
+		private int _n;
+		private double _gamma;
 
 		public Algoritm1(double p0, int n, double gamma)
 		{
@@ -32,18 +32,42 @@ namespace adc.Algoritms
 
 		public double[] Process()
 		{
-			var steps = (int)Math.Pow(2, _n); 
+			var steps = (int)Math.Pow(2, _n);
 			var result = new double[steps];
 			for (int i = 0; i < steps; i++)
 			{
-				result[i] = ProcessStep(i+1);
+				result[i] = ProcessStep(i + 1);
 			}
+			return result;
+		}
+
+		public Result Process1(double[] steps)
+		{
+			var result = new Result();
+
+			result.IdealQuantum = steps[steps.Length] / (double)steps.Length;
+
+			result.DeltaQuantums = new double[steps.Length];
+			var prevStep = 0d;
+			for (int i = 0; i < steps.Length; i++)
+			{
+				result.DeltaQuantums[i] = result.IdealQuantum - (steps[i] - prevStep);
+				prevStep = steps[i];
+			}
+
+			result.MaxDeltaQuantum = result.DeltaQuantums.Max( q => Math.Abs(q));
+
 			return result;
 		}
 
 		public class Result
 		{
-			public double[] Steps { get; private set; }
+			public double IdealQuantum { get; internal set; }
+			/// <summary>
+			/// Разница квантов с идеальным
+			/// </summary>
+			public double[] DeltaQuantums { get; internal set; }
+			public double MaxDeltaQuantum { get; internal set; }
 		}
 	}
 }
