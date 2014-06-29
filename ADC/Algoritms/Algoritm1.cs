343eca0d3b4ce661c4d03800fec43bd10d723cbf
unknown <Simeon Scherbinin>
2014-06-29 14:21:53 +0400

    add project with steps algoritm

---
+++ ADC/Algoritms/Algoritm1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adc.Algoritms
+{
+	public class Algoritm1
+	{
+		private  double _p0;
+		private  int _n;
+		private  double _gamma;
+
+		public Algoritm1(double p0, int n, double gamma)
+		{
+			_p0 = p0;
+			_n = n;
+			_gamma = gamma;
+		}
+
+		public double ProcessStep(int a)
+		{
+			double summ = 0;
+			for (int i = 0; i <= _n; i++)
+			{
+				var ai = (double)((a >> (_n - i)) & 0x1);
+				summ += ai * Math.Pow(2, -i + _gamma);
+			}
+			return _p0 * summ;
+		}
+
+		public double[] Process()
+		{
+			var steps = (int)Math.Pow(2, _n); 
+			var result = new double[steps];
+			for (int i = 0; i < steps; i++)
+			{
+				result[i] = ProcessStep(i+1);
+			}
+			return result;
+		}
+
+		public class Result
+		{
+			public double[] Steps { get; private set; }
+		}
+	}
+}
---
+++ adc.tests/Algorim1Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using adc.Algoritms;
+
+namespace adc.tests
+{
+	[TestClass]
+	public class Algorim1Test
+	{
+		[TestMethod]
+		public void ProcessStepTest()
+		{
+			var n = 2;
+			var p0 = 2;
+			var gamma = 4;
+			Assert.AreEqual(8, new Algoritm1(p0, n, gamma).ProcessStep(1));
+			Assert.AreEqual(16, new Algoritm1(p0, n, gamma).ProcessStep(2));
+		}
+
+		[TestMethod]
+		public void ProcessTest()
+		{
+			var n = 3;
+			var p0 = 2;
+			var gamma = 4;
+			var result = new Algoritm1(p0, n, gamma).Process();
+			Assert.AreEqual(8, result.Length);
+			Assert.AreEqual(4, result[0]);
+			Assert.AreEqual(8, result[1]);
+			Assert.AreEqual(12, result[2]);
+		}
+	}
+}
