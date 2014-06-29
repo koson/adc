ed661696c3b6e1ed1828f5302b171e5ae89a591d
unknown <Simeon Scherbinin>
2014-06-29 23:50:57 +0400

    add test for algoritms

--- adc.tests/Algorim1Test.cs
+++ adc.tests/Algorim1Test.cs
@@ -29,5 +29,52 @@ namespace adc.tests
 			Assert.AreEqual(8, result[1]);
 			Assert.AreEqual(12, result[2]);
 		}
+
+		[TestMethod]
+		public void Process1Test()
+		{
+			var n = 3;
+			var p0 = 2;
+			var gamma = 0.2;
+			var algoritm = new Algoritm1(p0, n, gamma);
+			var steps = algoritm.Process();
+			var result = algoritm.Process1(steps);
+			Assert.AreEqual(0.00000000000000016653345369377348, result.MaxDeltaQuantum);
+			Assert.AreEqual(0.28717458874925877, result.IdealQuantum);
+			var expect = new double[8]{ 
+				0.0
+				, 0.00000000000000011102230246251565
+				, 0.000000000000000055511151231257827
+				, 0.000000000000000055511151231257827
+				, 0.000000000000000055511151231257827
+				, 0.000000000000000055511151231257827
+				, -0.00000000000000016653345369377348
+				, -0.00000000000000016653345369377348
+			};
+			CollectionAssert.AreEqual(expect, result.DeltaQuantums);
+		}
+
+		[TestMethod]
+		public void Process2Test()
+		{
+			var n = 3;
+			var p0 = 2;
+			var gamma = 0.2;
+			var algoritm = new Algoritm1(p0, n, gamma);
+			var steps = algoritm.Process();
+			var result = algoritm.Process2(steps);
+			Assert.AreEqual(0.00000000000000044408920985006262, result.MaxDelta);
+			var expect = new double[8]{ 
+				0.0
+				, -0.00000000000000011102230246251565
+				, -0.00000000000000022204460492503131
+				, -0.00000000000000022204460492503131
+				, -0.00000000000000022204460492503131
+				, -0.00000000000000044408920985006262
+				, 0.0
+				, 0.0
+			};
+			CollectionAssert.AreEqual(expect, result.Deltas);
+		}
 	}
 }
