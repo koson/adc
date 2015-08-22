2cda782750b1e6413b7d3d2a92656074a910e4ac
unknown <s-scherbinin@parcsis.org>
2015-08-22 19:08:05 +0400

    refactoring

--- adc2/Algoritm.cs
+++ adc2/Algoritm.cs
@@ -105,57 +105,5 @@ namespace adc.core
 			continuousUniform.Samples(result);
 			return result;
 		}
-
-		/*
-		public Result2 Process2(double[] steps)
-		{
-			var result = new Result2();
-
-			result.Deltas = new double[steps.Length];
-
-			var max = steps[steps.Length - 1];
-			for (int i = 0; i < steps.Length; i++)
-			{
-				result.Deltas[i] = steps[i] - ((i + 1d) * max) / (double)steps.Length;
-			}
-
-			result.MaxDelta = result.Deltas.Max(d => Math.Abs(d));
-
-			return result;
-		}
-
-		public Result Process1(double[] steps)
-		{
-			var result = new Result();
-
-			result.IdealQuantum = steps[steps.Length - 1] / (double)steps.Length;
-
-			result.DeltaQuantums = new double[steps.Length];
-			var prevStep = 0d;
-			for (int i = 0; i < steps.Length; i++)
-			{
-				result.DeltaQuantums[i] = result.IdealQuantum - (steps[i] - prevStep);
-				prevStep = steps[i];
-			}
-
-			result.MaxDeltaQuantum = result.DeltaQuantums.Max(q => Math.Abs(q));
-
-			return result;
-		}
-		public class Result
-		{
-			public double IdealQuantum { get; internal set; }
-			/// <summary>
-			/// Разница квантов с идеальным
-			/// </summary>
-			public double[] DeltaQuantums { get; internal set; }
-			public double MaxDeltaQuantum { get; internal set; }
-		}
-
-		public class Result2
-		{
-			public double[] Deltas { get; internal set; }
-			public double MaxDelta { get; internal set; }
-		}*/
 	}
 }
